5a4c77c9097b5189c1696bb74bee48aa2c29113a
Bhuvaneswari Santharam <31025851+Bhuvaneswari-Santharam@users.noreply.github.com>
2019-04-11 21:55:34 -0700

    Update aksEngineUpgrade.sh

--- AKSEngine-E2E/AksTest-Scripts/aksEngineUpgrade.sh
+++ AKSEngine-E2E/AksTest-Scripts/aksEngineUpgrade.sh
@@ -84,7 +84,6 @@ fi
 
 # Basic details of the system
 log_level -i "Running  script as : $(whoami)"
-
 log_level -i "System information: $(sudo uname -a)"
 
 
@@ -95,6 +94,9 @@ log_level -i "Getting Resource group and region"
 
 export RESOURCE_GROUP=`ls -dt1 _output/* | head -n 1 | cut -d/ -f2`
 export REGION=`ls -dt1 _output/* | head -n 1 | cut -d/ -f2 | cut -d- -f2`
+
+echo "REGION: $REGION"
+echo "RESOURCE GROUP: $RESOURCE_GROUP"
 
 if [ $RESOURCE_GROUP == "" ] ; then
     log_level -i "Resource group not found.Upgrade can not be performed"
@@ -115,10 +117,6 @@ FQDN_ENDPOINT_SUFFIX=$(cat $ROOT_PATH/_output/$APIMODEL_FILE | jq '.properties.c
 IDENTITY_SYSTEM=$(cat $ROOT_PATH/_output/$APIMODEL_FILE | jq '.properties.customCloudProfile.identitySystem' | tr -d '"')
 AUTH_METHOD=$(cat $ROOT_PATH/_output/$APIMODEL_FILE | jq '.properties.customCloudProfile.authenticationMethod' | tr -d '"')
 AZURE_ENV_OLD=$(cat $ROOT_PATH/_output/$APIMODEL_FILE | jq '.properties.customCloudProfile.environment.name' | tr -d '"')
-
-
-
-echo "CLIENT_ID: $CLIENT_ID"
 
 if [ $CLIENT_ID == "" ] ; then
     log_level -i "Client ID not found.Upgrade can not be performed"
@@ -183,10 +181,18 @@ else
         --upgrade-version $UPGRADE_VERSION || exit 1
 fi
 
+log_level -i "Upgrading kubernetes cluster to version $UPGRADE_VERSION completed.Running E2E test..."
+
 cd $ROOT_PATH
 export CLUSTER_DEFINITION=$AKSENGINE_APIMODEL
 export CLEANUP_ON_EXIT=false
-export NAME
+export NAME=$RESOURCE_GROUP
+export CLIENT_ID=$CLIENT_ID
+export TENANT_ID=$TENANT_ID
+export SUBSCRIPTION_ID=$SUBSCRIPTION_ID
+export TIMEOUT=20m
+export REGION=$REGION
+
 set +e
 make test-kubernetes > upgrade_test_results
 set -e
@@ -194,7 +200,7 @@ set -e
 RESULT=$?
 log_level -i "Result: $RESULT"
 
-
+#Script returns exit=0 for the failure in the E2E test else throws the error code
 if [ $RESULT -lt 3 ] ; then
     exit 0
 else
