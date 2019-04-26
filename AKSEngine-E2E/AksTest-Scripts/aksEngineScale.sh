d71a343a6987621793618af54455d1a1f79537ca
Bhuvaneswari Santharam <31025851+Bhuvaneswari-Santharam@users.noreply.github.com>
2019-04-26 11:03:02 -0700

    Update aksEngineScale.sh

--- AKSEngine-E2E/AksTest-Scripts/aksEngineScale.sh
+++ AKSEngine-E2E/AksTest-Scripts/aksEngineScale.sh
@@ -256,7 +256,7 @@ export PORTAL_ENDPOINT=$ENDPOINT_PORTAL
 export GOPATH=/home/azureuser
 export GOROOT=/home/azureuser/bin/go
 export PATH=$GOPATH:$GOROOT/bin:$PATH
-make bootstrap
+sudo make bootstrap
 make validate-dependencies
 set +e
 make test-kubernetes > scale_test_results
