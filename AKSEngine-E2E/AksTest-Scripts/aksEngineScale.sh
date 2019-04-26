0cca0ae5d7478665543e838c6eb159d184cce9c0
Bhuvaneswari Santharam <31025851+Bhuvaneswari-Santharam@users.noreply.github.com>
2019-04-26 11:01:15 -0700

    Update aksEngineScale.sh

--- AKSEngine-E2E/AksTest-Scripts/aksEngineScale.sh
+++ AKSEngine-E2E/AksTest-Scripts/aksEngineScale.sh
@@ -124,7 +124,7 @@ fi
 
 cd $ROOT_PATH/_output
 
-sudo chown -R azureuser /home/azureuser/src
+sudo chown -R azureuser /home/azureuser/src/github.com
 
 CLIENT_ID=$(cat $ROOT_PATH/_output/$RESOURCE_GROUP/apimodel.json | jq '.properties.servicePrincipalProfile.clientId'| tr -d '"')
 FQDN_ENDPOINT_SUFFIX=$(cat $ROOT_PATH/_output/$RESOURCE_GROUP/apimodel.json | jq '.properties.customCloudProfile.environment.resourceManagerVMDNSSuffix' | tr -d '"')
