4e0003a9b5a70c5c4b1f4a5865a4e2f483f56071
Bhuvaneswari Santharam <31025851+Bhuvaneswari-Santharam@users.noreply.github.com>
2019-04-28 19:44:20 -0700

    Update aksEngineScale.sh

--- AKSEngine-E2E/AksTest-Scripts/aksEngineScale.sh
+++ AKSEngine-E2E/AksTest-Scripts/aksEngineScale.sh
@@ -253,7 +253,7 @@ export PORTAL_ENDPOINT=$ENDPOINT_PORTAL
 #####################################################################################
 #Section to install Go.
 
-cd /home/azureuser
+cd /home/azureuser/bin
 sudo wget https://dl.google.com/go/go1.11.4.linux-amd64.tar.gz
 sudo tar -C  home/azureuser/bin -xzf go1.11.4.linux-amd64.tar.gz
 
