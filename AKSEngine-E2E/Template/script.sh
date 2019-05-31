a06cdea048e16dfbcf9d68d85c4841a02ef113d6
Bhuvaneswari Santharam <31025851+Bhuvaneswari-Santharam@users.noreply.github.com>
2019-05-31 12:38:25 -0700

    Update script.sh

--- AKSEngine-E2E/Template/script.sh
+++ AKSEngine-E2E/Template/script.sh
@@ -235,7 +235,7 @@ sudo mv $ROOT_PATH/aks-engine $ROOT_PATH/src/github.com/Azure
 #Section to get the apimodel file
 log_level -i "Getting api model file"
 
-DEFINITION_TEMPLATE=$ROOT_PATH/src/github.com/Azure/aks-engine/examples/azurestack/$AKSENGINE_API_MODEL.json
+DEFINITION_TEMPLATE=$ROOT_PATH/src/github.com/Azure/aks-engine/examples/azure-stack/$AKSENGINE_API_MODEL.json
 
  if [ ! -f $DEFINITION_TEMPLATE ]; then
     log_level -e "API model template not found in expected location"
